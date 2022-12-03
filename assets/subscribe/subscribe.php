3b515eb08906cda9082988618fd93ac554bb2e5e
OmarSlimWhite <omarslimwhite@gmail.com>
2022-12-03 02:17:01 -0500

    first commit

--- assets/subscribe/subscribe.php
+++ assets/subscribe/subscribe.php
@@ -1,7 +1,26 @@
 <?php
 // Note: filter_var() requires PHP >= 5.2.0
 if ( isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
-      $e_mail = $_POST['email'] . "," . "\n";
-	  file_put_contents('email-list.txt', $e_mail, FILE_APPEND | LOCK_EX);
+     //$e_mail = $_POST['email'] . "," . "\n";
+	 // file_put_contents('email-list.txt', $e_mail, FILE_APPEND | LOCK_EX);
+    
+    
+    
+
+
+$myemail = 'omarwhitetrust@gmail.com';
+
+$email = $_POST['email'];
+
+
+$to = $myemail;
+$email_subject = "FREE BOOK";
+$email_body = "You have received a new request. Details are given below. \n Email: $email";
+$headers = "From: $email";
+
+mail($to, $email_subject, $email_body, $headers);
+
+
+
 }
 ?>
